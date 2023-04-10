a2a853bc052858d6e2b341834a4398d7b32ea928
Estifanos <estifanosbeyene21@gmail.com>
2023-04-10 14:48:57 -0400

    feat: add simple nav component

---
+++ src/lib/nav.svelte
@@ -0,0 +1,9 @@
+<header class="container">
+	<nav>
+		<ul><li><a href="/">OdinBuk</a></li></ul>
+		<ul>
+			<li><a href="/sign-in">Sign in</a></li>
+			<li><a href="/sign-up">Sign up</a></li>
+		</ul>
+	</nav>
+</header>
