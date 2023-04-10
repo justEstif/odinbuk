1cb9c0afada87433429f587e5005f92003d03588
Estifanos <estifanosbeyene21@gmail.com>
2023-04-10 14:48:44 -0400

    feat: add basic sign in and sign up pages

---
+++ src/routes/sign-in/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	export let form: { message?: string };
+</script>
+
+<svelte:head>
+	<title>OdinBuk | Sign in</title>
+</svelte:head>
+
+<h2>Sign in</h2>
+
+<form method="post" use:enhance>
+	<label for="username">
+		Username
+		<input type="text" id="username" name="username" placeholder="Username" required />
+	</label>
+	<label for="password">
+		Password
+		<input type="password" id="password" name="password" placeholder="password" required />
+	</label>
+	<button type="submit">Submit</button>
+</form>
+{#if form?.message}
+	<p class="error">{form.message || ''}</p>
+{/if}
+<a href="/sign-up" class="secondary" role="button">Sign up</a>
+<a href="/api/oauth?provider=github" class="contrast" role="button">Continue with Github</a>
---
+++ src/routes/sign-up/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	export let form: { message?: string };
+</script>
+
+<svelte:head>
+	<title>OdinBuk | Sign up</title>
+</svelte:head>
+
+<h2>Create an account</h2>
+
+<form method="post" use:enhance>
+	<label for="username">
+		Username
+		<input type="text" id="username" name="username" placeholder="Username" required />
+	</label>
+	<label for="password">
+		Password
+		<input type="password" id="password" name="password" placeholder="password" required />
+	</label>
+	<button type="submit">Submit</button>
+</form>
+{#if form?.message}
+	<p class="error">{form.message || ''}</p>
+{/if}
+
+<div class="grid">
+	<a href="/sign-in" class="secondary" role="button">Sign in</a>
+	<a href="/api/oauth?provider=github" class="contrast" role="button">Continue with Github</a>
+</div>
