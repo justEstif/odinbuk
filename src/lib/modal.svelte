3950198b92e8b1ccc3dbaeb607dec48e5befdf24
Estifanos <estifanosbeyene21@gmail.com>
2023-04-11 19:55:36 -0400

    feat: add basic svelte modal

---
+++ src/lib/modal.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let showModal: boolean;
+	let dialog: HTMLDialogElement;
+	$: if (dialog && showModal) dialog.showModal();
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}>
+	<div on:click|stopPropagation>
+		<slot />
+	</div>
+</dialog>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	import Modal from '$lib/modal.svelte';
+	let showModal = false;
+</script>
+
 <svelte:head>
 	<title>OdinBuk | Home</title>
 </svelte:head>
@@ -7,11 +12,44 @@
 	<summary>Create a post</summary>
 	<p>Hey create post using this form</p>
 </details>
-<article>
-	<header>user who post and go to user page on click</header>
-	body of post and go to post page on click
+
+<figure>
+	<figcaption>
+		<a href="#userpage">@username</a>
+		<small>2h</small>
+	</figcaption>
+	<img src="https://placehold.co/600x400" alt="Architecture" />
+
 	<footer>
-		<a href="#like">Like</a>
-		<a href="#comment">Comment</a>
+		<a href="#like">
+			<iconify-icon icon="fa-regular:heart" />
+		</a>
+		<button on:click={() => (showModal = true)}>
+			<iconify-icon icon="fa-regular:comment-dots" />
+		</button>
 	</footer>
-</article>
+</figure>
+
+<Modal bind:showModal>
+	<article>
+		<h3>Confirm your action!</h3>
+		<p>
+			Mauris non nibh vel nisi sollicitudin malesuada. Donec ut sagittis erat. Praesent eu eros
+			felis. Ut consectetur placerat pulvinar.
+		</p>
+		<footer>
+			<button on:click={() => (showModal = false)}>Cancel</button>
+		</footer>
+	</article>
+</Modal>
+
+<style>
+	footer {
+		padding: 10px 0px;
+		display: flex;
+		gap: var(--grid-spacing-horizontal);
+	}
+	iconify-icon {
+		font-size: 20px;
+	}
+</style>
