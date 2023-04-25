aa5b0e4ac9e01a98408bcabefea4f0186495afca
Estifanos <estifanosbeyene21@gmail.com>
2023-04-25 11:14:26 -0400

    feat: setup user auth with db

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,17 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
 import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import db from '$lib/server/db';
 
 export const handle = SvelteKitAuth({
+	adapter: PrismaAdapter(db),
+	session: {
+		strategy: 'database',
+		generateSessionToken: () => {
+			return crypto.randomUUID();
+		}
+	},
 	providers: [
 		GitHub({
 			clientId: GITHUB_ID,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <h1>Home</h1>
 
-<!-- {#if $page.data.session}
+{#if $page.data.session}
 	<h1>Protected page</h1>
 	<p>This is a protected content. You can access this content because you are signed in.</p>
 	<p>Session expiry: {$page.data.session?.expires}</p>
@@ -13,4 +13,4 @@
 	<p>
 		<a href="/auth/signin"> You must be signed in to view this page </a>
 	</p>
-{/if} -->
+{/if}
